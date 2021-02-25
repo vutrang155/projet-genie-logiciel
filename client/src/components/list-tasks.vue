eb665d272c62b4a3dfe83052cc0eb63c988272ba
Yugo-Kiyoshi <72647828+Yugo-Kiyoshi@users.noreply.github.com>
2021-02-25 23:39:26 +0100

    Update list-tasks.vue

---
+++ client/src/components/list-tasks.vue
@@ -0,0 +1,16 @@
+<template>
+	<ul>
+    <li v-for="task in tasks" >
+      <div v-bind:class="task.state" >
+        <b-container>
+          <b-row>
+            <b-col> Projet: {{task.project}} </b-col>
+            <b-col> Responsable: {{task.responsable}} </b-col>
+            <b-col> Avancement: {{task.avancement}} </b-col>
+          </b-row>
+        </b-container>
+
+      </div>
+    </li>
+  </ul>
+</template>

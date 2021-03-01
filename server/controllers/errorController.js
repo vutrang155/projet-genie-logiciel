2dc7fecf89e51447358c4ad89bd9c591931c470b
Hoang Phong Vu TRANG <tranghoangphongvu@gmail.com>
2021-03-01 18:41:05 +0100

    add errorController

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -5,14 +5,42 @@ const Projet = require("../models/Projet");
 const User = require("../models/User");
 const Tache = require("../models/Tache");
 
+const ERROR_CODE = 500;
 
 exports.checkUser = async(id)=>{
     var foundId =await User.find({ _id:id  });
     if (foundId === undefined || id.length  === 0) {
         const error = new Error("User Unknown");
-        error.statusCode = 404;
+        error.statusCode = ERROR_CODE;
         throw error;
     }
     console.log("User existe")
 }
+exports.checkUserByNomUtilisateur = async(id)=>{
+    var foundId =await User.find({ nomUtilisateur:id  });
+    if (foundId === undefined || id.length  === 0) {
+        const error = new Error("User Unknown");
+        error.statusCode = ERROR_CODE;
+        throw error;
+    }
+    console.log("User existe")
+}
+exports.checkTache = async(id)=> {
+    var foundId =await Tache.find({ _id:id  });
+    if (foundId === undefined || id.length  === 0) {
+        const error = new Error("Tache id not found");
+        error.statusCode = ERROR_CODE;
+        throw error;
+    }
+    console.log("Tache existe")
+}
 
+exports.checkProjet = async(id)=> {
+    var foundId =await Projet.find({ _id:id  });
+    if (foundId === undefined || id.length  === 0) {
+        const error = new Error("Projet id not found");
+        error.statusCode = ERROR_CODE;
+        throw error;
+    }
+    console.log("Projet existe")
+}

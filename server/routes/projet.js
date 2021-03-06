7d0d0c9672741c1e6667d47760eab75192483043
Vathanalakshan <Vathanan@outlook.com>
2021-03-07 00:02:57 +0100

    Commenter le code

--- server/routes/projet.js
+++ server/routes/projet.js
@@ -2,13 +2,19 @@ const express = require("express");
 const router = express.Router();
 const projetController = require("../controllers/projetController");
 
+//Route pour creer un projet
 router.post("/create", projetController.create);
-
+//Route pour supprimer un projet via son id
 router.delete("/delete/:projetId", projetController.delete);
+//Route pour mettre a jour un projet
 router.put("/update", projetController.update);
+//Route pour recuperer tous les projets
 router.get("/getAll", projetController.getAll);
+//Route pour recuperer un projet via son id
 router.get("/getById/:projetId", projetController.getById);
+//Route pour recuperer des projet d'un utilisateur utilisant des userid
 router.get("/getByUser/:userId", projetController.getByUser);
+//Route pour recuperer des projet d'un utilisateur utilisant des userid
 router.get("/getByResponsable/:responsableId",projetController.getByResponsable);
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,13 +2,20 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-
+//Route pour les utilisateur se connecte
 router.post("/login", userController.login);
+//Route pour que les utilisateur s'enregistre
 router.post("/new",  userController.register);
+//Route pour recuperer tous les utilisateurs
 router.get("/getAll",userController.getall);
+//Route pour supprimer un utilisateur avec un id
 router.delete("/delete/:userId", userController.delete);
+//Route pour mettre a jour un utilisateur
 router.put("/update", userController.update);
+//Route pour recuperer un utilisateur via son nom utilisateur
 router.get("/getByNomUtilisateur/:nomUtilisateur", userController.getByNomUtilisateur);
+//Route pour recuperer un utilisateur via son id
 router.get("/getById/:id", userController.getById);
+//Route pour recuperer des utilisateur via son type
 router.get("/getByType/:role", userController.getByType);
 module.exports = router;
